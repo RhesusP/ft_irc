9ceabfddb4158191d0df2b1b8cf52e1f4fdd8e6f
cbernot <camil.bernot.cb@gmail.com>
2024-01-18 19:35:52 +0100

    Finish setPort() server function

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:45:40 by cbernot           #+#    #+#             */
-/*   Updated: 2024/01/18 14:40:42 by cbernot          ###   ########.fr       */
+/*   Updated: 2024/01/18 19:31:28 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,22 +49,21 @@ void Server::setPassword(std::string const &password)
 	this->_password = password;
 }
 
-// TODO wip
-void Server::setPassword(std::string const &port)
+void Server::setPort(std::string const &port)
 {
 	int p;
 	size_t len = port.size();
 	if (len < 4 || len > 6)
-		return -1;
+		throw BadPortException();
 	for (size_t i = 0; i < len; i++)
 	{
-		if (!isdigit(p[i]))
-			return -1;
+		if (!isdigit(port[i]))
+			throw BadPortException();
 	}
-	port = std::stoi(p);
-	if (port < 1024 || port > 65535)
-		return -1;
-	return port;
+	p = std::stoi(port);
+	if (p < 1024 || p > 65535)
+		throw BadPortException();
+	this->_port = p;
 }
 
 std::string Server::getPassword(void) const
