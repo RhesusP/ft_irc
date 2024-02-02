a8e18caf3ec544d1f54146d9c7cf4aa11f4fd45c
cbernot <camil.bernot.cb@gmail.com>
2024-02-02 17:17:41 +0100

    wip

--- inc/Message.hpp
+++ inc/Message.hpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 18:14:43 by cbernot           #+#    #+#             */
-/*   Updated: 2024/02/01 15:23:55 by cbernot          ###   ########.fr       */
+/*   Updated: 2024/02/02 14:58:53 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ enum command
 	PONG,
 	OPER,
 	QUIT,
+	PRIVMSG,
 
 	KICK,
 	INVITE,
@@ -46,18 +47,26 @@ private:
 	std::string _raw;
 	std::map<std::string, std::string> _tags;
 	std::string _source;
-	std::string _response;
 	command _command;
 	std::vector<std::string> _parameters;
+	std::string _response;
 
 	std::string getTags(std::string const &raw);
 	std::string getSource(std::string const &raw);
 	std::string getCommand(std::string const &raw);
+	void getParameters(std::string const & raw);
 
 public:
 	Message(void);
 	Message(std::string const &raw);
 	~Message(void);
+
+	std::map<std::string, std::string> & getTags(void);
+	std::string const & getSource(void);
+	command	getCommand(void);
+	std::vector<std::string> getParameters(void);
 };
+
+std::ostream &operator<<(std::ostream &o, Message &rhs);
 
 #endif
--- src/Message.cpp
+++ src/Message.cpp
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 18:17:28 by cbernot           #+#    #+#             */
-/*   Updated: 2024/02/01 15:33:19 by cbernot          ###   ########.fr       */
+/*   Updated: 2024/02/02 15:27:26 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ const char *BadTagException::what() const throw()
 
 command stocmd(std::string const &str)
 {
+	std::cout << "command to assign: " << str << std::endl;
 	if (str == "CAP")
 		return CAP;
 	else if (str == "PASS")
@@ -43,8 +44,59 @@ command stocmd(std::string const &str)
 		return TOPIC;
 	else if (str == "MODE")
 		return MODE;
+	else if (str == "PRIVMSG")
+		return PRIVMSG;
 	else
 		return UNKNOWN;
+}
+
+std::string printCmd(command cmd)
+{
+	switch (cmd)
+	{
+	case CAP:
+		return "CAP"; 
+		break;
+	case PASS:
+		return "PASS"; 
+		break;
+	case NICK:
+		return "NICK"; 
+		break;
+	case USER:
+		return "USER"; 
+		break;
+	case PING:
+		return "PING"; 
+		break;
+	case PONG:
+		return "PONG"; 
+		break;
+	case OPER:
+		return "OPER"; 
+		break;
+	case QUIT:
+		return "QUIT"; 
+		break;
+	case KICK:
+		return "KICK"; 
+		break;
+	case INVITE:
+		return "INVITE"; 
+		break;
+	case TOPIC:
+		return "TOPIC"; 
+		break;
+	case MODE:
+		return "MODE"; 
+		break;
+	case PRIVMSG:
+		return "PRIVMSG";
+		break;
+	default:
+		return "UNKNOWN"; 
+		break;
+	}
 }
 
 Message::Message(void)
@@ -93,8 +145,8 @@ std::string Message::getSource(std::string const &raw)
 std::string Message::getCommand(std::string const &raw)
 {
 	int endpos = raw.find(" ");
-	std::string str = raw.substr(1, endpos - 1);
-	if (str.size() == 3)
+	std::string str = raw.substr(0, endpos);
+	if (str.size() == 3 && isdigit(str[0]))	//TODO /!\ To handle
 	{
 		for (int i = 0; i < str.size(); i++)
 		{
@@ -109,6 +161,7 @@ std::string Message::getCommand(std::string const &raw)
 			if (!isalpha(str[i]))
 				throw BadCommandException();
 		}
+		_command = stocmd(str);
 	}
 	std::string rawCopy = raw;
 	rawCopy.erase(0, endpos + 1);
@@ -117,8 +170,8 @@ std::string Message::getCommand(std::string const &raw)
 
 void getParameters(std::string const &raw)
 {
-	//TODO need to split raw by space delimiter and remove extra spaces
-	//TODO handle trailing ? (https://modern.ircdocs.horse/#parameters)
+	// TODO need to split raw by space delimiter and remove extra spaces
+	// TODO handle trailing ? (https://modern.ircdocs.horse/#parameters)
 }
 
 Message::Message(std::string const &raw)
@@ -131,7 +184,8 @@ Message::Message(std::string const &raw)
 		_raw = getTags(_raw);
 		_raw = getSource(_raw);
 		_raw = getCommand(_raw);
-		getParameters(_raw);
+		// getParameters(_raw);
+		std::cout << *this << std::endl;
 	}
 	catch (std::exception &e)
 	{
@@ -140,3 +194,46 @@ Message::Message(std::string const &raw)
 }
 
 Message::~Message(void) {}
+
+std::map<std::string, std::string> &Message::getTags(void)
+{
+	return _tags;
+}
+
+std::string const &Message::getSource(void)
+{
+	return _source;
+}
+
+command Message::getCommand(void)
+{
+	return _command;
+}
+
+std::vector<std::string> Message::getParameters(void)
+{
+	return _parameters;
+}
+
+std::ostream &operator<<(std::ostream &o, Message &rhs)
+{
+	std::map<std::string, std::string> tags = rhs.getTags();
+	std::string source = rhs.getSource();
+	command cmd = rhs.getCommand();
+	std::vector<std::string> params = rhs.getParameters();
+
+	std::cout << "============ NEW MESSAGE ============" << std::endl;
+	std::cout << "\tTags: " << std::endl;
+	std::map<std::string, std::string>::iterator it_beg_tags = tags.begin();
+	std::map<std::string, std::string>::iterator it_end_tags = tags.end();
+	while (it_beg_tags != it_end_tags)
+	{
+		std::cout << "\t\t" << it_beg_tags->first << "=" << it_beg_tags->second << std::endl;
+		++it_beg_tags;
+	}
+	std::cout << "\tSource: " << source << std::endl;
+	std::cout << "\tCommand:" << printCmd(cmd) << std::endl;
+	for (size_t i = 0; i < params.size(); i++)
+		std::cout << "\t\t" << params[i] << std::endl;
+	return o;
+}
